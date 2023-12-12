19f8188b3b77574df01945d0cde96f677df7d452
YVolkov404 <sir.volkov@outlook.com>
2023-12-12 00:55:12 +0000

    Deploying to gh-pages from @ YVolkov404/goit-react-hw-05-movies@3e669d012af180d9ffb5b3e9af5b952ca61513b6 🚀

---
+++ static/js/692.15497a54.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[692],{7692:function(t,a,e){e.d(a,{gew:function(){return i}});var h=e(9983);function i(t){return(0,h.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M4 21h1V8H4a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2zM20 8h-7l1.122-3.368A2 2 0 0 0 12.225 2H12L7 7.438V21h11l3.912-8.596L22 12v-2a2 2 0 0 0-2-2z"}}]})(t)}}}]);
+//# sourceMappingURL=692.15497a54.chunk.js.map
